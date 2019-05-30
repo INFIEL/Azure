c7dd187d0d38ea9a6be111db97ce708fb4e0a841
Andre_Luiz <andre.lima@socialminer.com>
2019-05-29 21:26:12 -0300

    Adicionar arquivos de projeto.

---
+++ Azure/Azure/AuthenticateDroid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Droid;
+using Microsoft.WindowsAzure.MobileServices;
+
+
+[assembly: Xamarin.Forms.Dependency(typeof(AuthenticateDroid))]
+
+namespace Azure.Droid
+{
+ public class AuthenticateDroid : IAuthenticate
+    {
+        public async Task<MobileServiceUser> Authenticate(MobileServiceClient client, MobileServiceAuthenticationProvider provider)
+        {
+            try
+            {
+              //  return await client.LoginAsync(Xamarin.Forms.Forms.Context, provider);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ Azure/Azure/IAuthenticate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace Azure
+{
+  public  interface IAuthenticate
+    {
+        Task<MobileServiceUser> Authenticate(MobileServiceClient client, MobileServiceAuthenticationProvider provider);
+    }
+}
